cb9a824ee6d3ae6964624caa1dd294d6007f221b
romanpalmin <roman.palmin@gmail.com>
2014-09-09 01:00:17 +0400

    Изменение шаблонов и исходных данных

--- public/js/templates/compiled/details.dust.js
+++ public/js/templates/compiled/details.dust.js
@@ -4,7 +4,7 @@
 (function() {
     dust.register("details_template", body_0);
     function body_0(chk, ctx) {
-        return chk.write("<table class='info'><tr  class='header'><td colspan='3'><br/>").reference(ctx.get("namerus"), ctx, "h").write(" (").reference(ctx.get("name"), ctx, "h").write(")<hr /</td></tr><tr><td rowspan='3' class='left'><img src = './img/").reference(ctx.get("img"), ctx, "h").write("'/></td><td  class='center'>№ п/п: <span class='right'>").reference(ctx.get("id"), ctx, "h").write("</span></td></tr><tr><td class='center'>Расстояние от солнца: <span class='right'>").reference(ctx.get("dist"), ctx, "h").write("</span></td></tr><tr><td class='center'>Вес: <span class='right'>").reference(ctx.get("weight"), ctx, "h").write("</span></td></tr><tr><td colspan='3'><hr /><div class = 'desc'>").reference(ctx.get("desc"), ctx, "h").write("</div></td></tr></table>");
+        return chk.write("<table class='info'><tr  class='header'><td colspan='3'><br/>").reference(ctx.get("namerus"), ctx, "h").write(" (").reference(ctx.get("name"), ctx, "h").write(")<hr /</td></tr><tr><td rowspan='3' class='left'><img src = './img/").reference(ctx.get("img"), ctx, "h").write("'/></td><td  class='center'>№ п/п от Солнца: <span class='right'>").reference(ctx.get("id"), ctx, "h").write("</span></td></tr><tr><td class='center'>Расстояние: <span class='right'>").reference(ctx.get("dist"), ctx, "h").write("</span></td></tr><tr><td class='center'>Масса: <span class='right'>").reference(ctx.get("weight"), ctx, "h").write("</span></td></tr><tr><td colspan='3'><hr /><div class = 'desc'>").reference(ctx.get("desc"), ctx, "h").write("</div></td></tr></table>");
     }
     return body_0;
 })();
